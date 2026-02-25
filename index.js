71fbaa047b8fa388e284252b42d705067e0fe0ad
wizardleeen <wizardleen@gmail.com>
2026-02-25 13:11:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Hello, World!" }));
+  res.end(JSON.stringify({ message: "Hello, Dingtalk!" }));
 });
 
 server.listen(PORT, () => {
